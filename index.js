c7f2f15d46df413e993830bb255971014f6722fb
Robby Kanyur <robby@kanyur.com>
2022-01-08 17:15:50 -0500

    Load usernames from file

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('howdy');
+const fs = require('fs').promises
+
+const loadNames = async () => {
+  const data = await fs.readFile('usernames.txt', 'utf-8')
+  return data.toString().split('\n')
+}
+
+const main = async () => {
+  const data = await loadNames()
+  const usernames = data.filter(x => x != '')
+  console.log(usernames)
+}
+
+main()
